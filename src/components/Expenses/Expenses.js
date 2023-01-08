be973cdf2304b746d1f37d29fefabbc2755e3e3e
Pak In Chan (Stanley) <78773070+stanleychan0507@users.noreply.github.com>
2023-01-08 13:35:29 +0100

    First commit

---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+
+import "./Expenses.css";
+import ExpenseItem from "./ExpenseItem";
+import ExpensesFilter from "./ExpensesFilter";
+import Card from "../UI/Card";
+
+const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState("2020");
+
+  const filterChangedHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
+  let expensesContent = <p>No expenses found.</p>;
+
+  if (filteredExpenses.length > 0) {
+    expensesContent = filteredExpenses.map((expense) => (
+      <ExpenseItem
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      />
+    ));
+  }
+
+  return (
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter
+          selected={filteredYear}
+          onChangedFilter={filterChangedHandler}
+        />
+        {expensesContent}
+      </Card>
+    </div>
+  );
+};
+
+export default Expenses;
