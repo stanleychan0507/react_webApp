ffbb2a733e3c51ff288a423c76635e5283eb14fc
Pak In Chan (Stanley) <78773070+stanleychan0507@users.noreply.github.com>
2023-01-21 16:43:33 -0800

    70_dynamicChart : routing between Expensese data and Displaying them on Chart Bar.

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 import "./Expenses.css";
 import ExpensesFilter from "./ExpensesFilter";
 import ExpensesList from './ExpensesList';
+import ExpensesChart from './ExpensesChart';
 import Card from "../UI/Card";
 
 const Expenses = (props) => {
@@ -23,6 +24,7 @@ const Expenses = (props) => {
           selected={filteredYear}
           onChangedFilter={filterChangedHandler}
         />
+        <ExpensesChart expenses={filteredExpenses} />
         <ExpensesList items={filteredExpenses}/>
       </Card>
     </div>
